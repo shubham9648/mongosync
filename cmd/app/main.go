9634a3c85146af3275d95d8b795ee8cc7ee31f77
shubham078 <stiwari@cognitus.com>
2024-07-22 11:50:18 +0530

    feat(internal/elasticsearch): added elastic connect

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+
+	es "github.com/shubham9648/mongosync/internal/elasticsearch"
 	"github.com/shubham9648/mongosync/internal/mongo"
 	"go.uber.org/zap"
 )
@@ -13,4 +16,8 @@ func main() {
 		zap.L().Error("error while connecting to mongoDB", zap.Error(err))
 		panic(err)
 	}
+	fmt.Println("client is ", client)
+
+	esClient, err := es.InitializeESClient()
+	fmt.Println("client is ", esClient)
 }
